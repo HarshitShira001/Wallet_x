11a937b76c19b7347c3fc67d274b6a71b4e5a0c2
Harshit Shira <shiraashu8@gmail.com>
2026-06-21 12:00:00 +0530

    chore(frontend): scaffold vite app and configure Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import Signin from "./pages/Signin";
+
+function App() {
+  return (
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
